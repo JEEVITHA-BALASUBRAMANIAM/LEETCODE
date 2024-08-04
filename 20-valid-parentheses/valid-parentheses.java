dc83077474ab776327c2bd24212e372395db9154
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-04 20:46:26 +0530

    Time: 49 ms (5.05%) | Memory: 44.9 MB (5.71%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
+            }
+        }
+    }
+}
