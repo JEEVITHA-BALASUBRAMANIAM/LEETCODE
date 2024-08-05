bd6b39f817a68eee159535eb49008341996a4bc1
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-05 14:09:29 +0530

    Time: 6 ms (70.10%) | Memory: 55 MB (34.50%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -15,11 +15,18 @@ public List<Integer> findDuplicates(int[] nums) {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        int[] nums = {4,3,2,7,8,2,3,1};
-        Solution solution = new Solution();
-        List<Integer> duplicates = solution.findDuplicates(nums);
-        System.out.println("Duplicates: " + duplicates);
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
