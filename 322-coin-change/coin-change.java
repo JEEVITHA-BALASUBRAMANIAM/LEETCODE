dec8da8ac98a4167a66e735082b119f5ad64811d
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-07 22:58:06 +0530

    Time: 30 ms (25.28%) | Memory: 45 MB (24.05%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,43 @@
+class Solution {
+   
+     public static int solve(int[] coins,int i,int j,int[][] dp)
+    {
+        if(i==0)
+        {
+            if(j%coins[0]==0)
+            {
+                return j/coins[i]; 
+            }
+            else 
+            {
+                return Integer.MAX_VALUE/2;
+            }
+        } 
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int take=Integer.MAX_VALUE/2,not_take; 
+        not_take = 0 + solve(coins,i-1,j,dp); 
+        if(coins[i]<=j)
+        {
+            take = 1+solve(coins,i,j-coins[i],dp); 
+        }
+           dp[i][j] = Math.min(take,not_take);  
+    
+
+           return dp[i][j]; 
+    }
+        public static int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int result = solve(coins, n - 1, amount, dp);
+        return (result == Integer.MAX_VALUE / 2) ? -1 : result;
+    }
+       
+    }
