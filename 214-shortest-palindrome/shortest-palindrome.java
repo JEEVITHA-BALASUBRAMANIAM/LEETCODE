92a5eb7a90248f4b73e446acd05a9f10c3277ae3
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-17 16:27:20 +0530

    Time: 578 ms (16.55%) | Memory: 44.3 MB (90.92%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,13 +1,44 @@
-class Solution {
+//class Solution {
+    //public String shortestPalindrome(String s) {
+        // StringBuilder r = new StringBuilder(s).reverse();
+        // for (int i = 0; i <s.length(); i++) {
+        //     if (s.startsWith(r.substring(i))) {
+        //         return r.substring(0, i) + s;
+        //     }
+        // }
+        // return ""; 
+//     }
+// }
+
+
+class Solution{
     public String shortestPalindrome(String s) {
-        StringBuilder r = new StringBuilder(s).reverse();
-        for (int i = 0; i <s.length(); i++) {
-            if (s.startsWith(r.substring(i))) {
-                return r.substring(0, i) + s;
+      if(s.length()==0){
+           return "";
+      }
+        int maxLen = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (solve(s, 0, i)) {
+                maxLen = i + 1;
+                break;
             }
         }
-        return ""; 
+
+        String suffix=s.substring(maxLen);
+        String prefix=new StringBuilder(suffix).reverse().toString();
+        return prefix+s;       
     }
+
+       public static boolean solve(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+      }
 }
 
 
@@ -19,6 +50,30 @@ class Solution {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
