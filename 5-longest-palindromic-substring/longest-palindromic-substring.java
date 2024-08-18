071a0b29481e9e17512dc84bf7931b0eb3dc7bba
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-18 06:31:45 +0530

    Time: 15 ms (87.88%) | Memory: 42.1 MB (85.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -54,36 +54,3 @@ class Solution {
 
 
 
-
-//         if (s.length()==0) {
-//             return "";
-//         }
-
-//         int start=0,end=0;
-//         for (int i=0;i<s.length();i++) {
-            
-//             int odd=solve(s,i,i);          
-//             int even=solve(s,i,i+1);        
-//             int len=Math.max(odd,even);
-          
-//             if (len>end-start) {
-//                 start=i-(len-1)/2;
-//                 end=i+len/2;
-//             }
-//         }     
-//         return s.substring(start,end+1);
-//     }
-   
-//     private int solve(String s,int left,int right) {
-        
-//         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
-             
-//                    left--;
-//                    right++;
-               
-//             }
-        
-       
-//         return right-left-1;  
-//     }
-// }
