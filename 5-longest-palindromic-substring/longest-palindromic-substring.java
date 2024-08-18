c4a2f2d21031136ebf4b7f6603ed4ea4ed96cce6
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-18 06:31:31 +0530

    Time: 15 ms (87.88%) | Memory: 42 MB (90.94%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,89 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length()==0) {
-            return "";
-        }
-
-        int start=0,end=0;
-        for (int i=0;i<s.length();i++) {
-            
-            int odd=solve(s,i,i);          
-            int even=solve(s,i,i+1);        
-            int len=Math.max(odd,even);
-          
-            if (len>end-start) {
+      if(s.length()==0){
+        return "";
+      }
+      int start=0,end=0;
+      for(int i=0;i<s.length();i++){
+        int odd= solve(s,i,i);
+        int even= solve(s,i,i+1);
+        int len=Math.max(odd,even);
+        if (len>end-start) {
                 start=i-(len-1)/2;
                 end=i+len/2;
             }
-        }     
-        return s.substring(start,end+1);
+
+      }
+      return s.substring(start,end+1);
     }
-   
-    private int solve(String s,int left,int right) {
-        
-        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
-             
-                   left--;
-                   right++;
-               
-            }
-        
-       
-        return right-left-1;  
+
+    public static int solve(String s,int left,int right){
+        while(left>=0 &&  right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right-left-1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         if (s.length()==0) {
+//             return "";
+//         }
+
+//         int start=0,end=0;
+//         for (int i=0;i<s.length();i++) {
+            
+//             int odd=solve(s,i,i);          
+//             int even=solve(s,i,i+1);        
+//             int len=Math.max(odd,even);
+          
+//             if (len>end-start) {
+//                 start=i-(len-1)/2;
+//                 end=i+len/2;
+//             }
+//         }     
+//         return s.substring(start,end+1);
+//     }
+   
+//     private int solve(String s,int left,int right) {
+        
+//         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+             
+//                    left--;
+//                    right++;
+               
+//             }
+        
+       
+//         return right-left-1;  
+//     }
+// }
