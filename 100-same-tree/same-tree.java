5b0f13525c65c933bbb7d8fc6c4fe13a0cf5eab1
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-08-23 16:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (67.18%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -20,7 +20,7 @@ class Solution {
         }
         if( p==null || q==null || p.val!=q.val)
         return false;
-
+        
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
