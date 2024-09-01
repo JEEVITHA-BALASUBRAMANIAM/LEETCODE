9a854d91700021ac0b15e1c197e8f45e555f972f
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2024-09-01 06:51:33 +0530

    Time: 3 ms (55.25%) | Memory: 42.1 MB (7.47%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+       Set<Character> set = new HashSet<>();      
+        for (char ch : sentence.toCharArray())
+            set.add(ch);
+        return set.size() == 26;        
+    }
+}
