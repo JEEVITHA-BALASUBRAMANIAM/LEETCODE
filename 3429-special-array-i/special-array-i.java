a2cbba3e445fcc58a2b56ce487c43c8ac191b88f
JEEVITHA-BALASUBRAMANIAM <145848476+JEEVITHA-BALASUBRAMANIAM@users.noreply.github.com>
2025-02-10 10:18:18 +0530

    Time: 1 ms (33.50%) | Memory: 43.4 MB (55.61%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -3,7 +3,7 @@ class Solution {
     if (nums.length <= 1) {
             return true;
         }
-    for (int i = 0; i < nums.length - 1; i++) {
+    for (int i = 0; i < nums.length-1; i++) {
               if ((nums[i]%2==0 && nums[i+1]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0)) {
                 return false;
             }
